c5da4b43ba8d4f67ecbcf8836cc6b128bfdf4cd8
Hernán L. Romero <romero.hernan.87@gmail.com>
2026-04-01 15:25:41 -0300

    adding json for test signatur

--- app/src/test/java/com/example/ualaapp/data/CoordinatesTest.kt
+++ app/src/test/java/com/example/ualaapp/data/CoordinatesTest.kt
@@ -1,5 +1,6 @@
 package com.example.ualaapp.data
 
+import com.example.ualaapp.TestUtils
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -25,5 +26,13 @@ class CoordinatesTest {
 
         assertEquals(-34.283333, coordinates.lat, 0.0)
         assertEquals(-44.549999, coordinates.lon, 0.0)
+    }
+
+    @Test
+    fun deserialize_json_coordinates() {
+        val coordinates = TestUtils.deserialize<Coordinates>("coordinates.json")
+
+        assertEquals(60.166672, coordinates.lat, 0.0)
+        assertEquals(24.41667, coordinates.lon, 0.0)
     }
 }
