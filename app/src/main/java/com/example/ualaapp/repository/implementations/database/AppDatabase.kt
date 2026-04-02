ee7a3684d8872b04e60e7f32ddf5915d2afa9703
Hernán L. Romero <romero.hernan.87@gmail.com>
2026-04-02 17:09:44 -0300

    adding database

---
+++ app/src/main/java/com/example/ualaapp/repository/implementations/database/AppDatabase.kt
@@ -0,0 +1,19 @@
+package com.example.ualaapp.repository.implementations.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.ualaapp.repository.implementations.database.daos.CityDao
+import com.example.ualaapp.repository.implementations.database.daos.UserDao
+import com.example.ualaapp.repository.implementations.database.entities.CityEntity
+import com.example.ualaapp.repository.implementations.database.entities.FavouriteEntity
+import com.example.ualaapp.repository.implementations.database.entities.UserEntity
+
+@Database(entities = [UserEntity::class, CityEntity::class, FavouriteEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun cityDao(): CityDao
+
+    abstract fun userDao(): UserDao
+
+    abstract fun favouriteDao(): UserDao
+}
