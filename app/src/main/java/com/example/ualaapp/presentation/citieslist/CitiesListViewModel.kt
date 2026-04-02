5d61009685b3e271daa9b6085ab3f3a1bba829e1
Hernán L. Romero <romero.hernan.87@gmail.com>
2026-04-02 16:00:06 -0300

    changes in viemodel

--- app/src/main/java/com/example/ualaapp/presentation/citieslist/CitiesListViewModel.kt
+++ app/src/main/java/com/example/ualaapp/presentation/citieslist/CitiesListViewModel.kt
@@ -1,26 +1,11 @@
 package com.example.ualaapp.presentation.citieslist
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.cachedIn
 import com.example.ualaapp.repository.CitiesApiService
-import com.example.ualaapp.repository.CityPagingSource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class CitiesListViewModel @Inject constructor(
     private val apiService: CitiesApiService
-) : ViewModel() {
-
-    val citiesFlow = Pager(
-        config = PagingConfig(
-            pageSize = 20,
-            prefetchDistance = 5,
-            enablePlaceholders = false
-        ),
-        pagingSourceFactory = { CityPagingSource(apiService) }
-    ).flow.cachedIn(viewModelScope)
-}
+) : ViewModel()
