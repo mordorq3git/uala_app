ec354d950a4ea8f6d91f27e49abb169fbf7b4f8a
Hernán L. Romero <romero.hernan.87@gmail.com>
2026-04-04 17:22:16 -0300

    save cities

--- app/src/main/java/com/example/ualaapp/repository/implementations/DataBaseRepositoryImpl.kt
+++ app/src/main/java/com/example/ualaapp/repository/implementations/DataBaseRepositoryImpl.kt
@@ -2,17 +2,19 @@ package com.example.ualaapp.repository.implementations
 
 import com.example.ualaapp.data.City
 import com.example.ualaapp.data.Coordinates
-import com.example.ualaapp.repository.Repository
+import com.example.ualaapp.repository.implementations.database.DatabaseRepository
 import com.example.ualaapp.repository.implementations.database.daos.CityDao
 import com.example.ualaapp.repository.implementations.database.daos.FavouriteDao
 import com.example.ualaapp.repository.implementations.database.daos.UserDao
+import com.example.ualaapp.repository.implementations.database.entities.CityEntity
+import com.example.ualaapp.repository.implementations.database.entities.CoordinatesEntity
 import javax.inject.Inject
 
 class DataBaseRepositoryImpl @Inject constructor(
     private val cityDao: CityDao,
     private val userDao: UserDao,
     private val favouriteDao: FavouriteDao
-) : Repository {
+) : DatabaseRepository {
     override suspend fun getCities(): List<City> {
         return cityDao.getAll().map { entity ->
             City(
@@ -25,5 +27,21 @@ class DataBaseRepositoryImpl @Inject constructor(
                 )
             )
         }
+    }
+
+    override suspend fun setCities(listOfCities: List<City>) {
+        val listOfCitiesEntities = listOfCities.map { city ->
+            CityEntity(
+                _id = city._id,
+                name = city.name,
+                country = city.country,
+                coord = CoordinatesEntity(
+                    lat = city.coord.lat,
+                    lon = city.coord.lon
+                )
+            )
+        }
+
+        cityDao.refreshData(listOfCitiesEntities)
     }
 }
--- app/src/main/java/com/example/ualaapp/repository/implementations/database/daos/CityDao.kt
+++ app/src/main/java/com/example/ualaapp/repository/implementations/database/daos/CityDao.kt
@@ -2,7 +2,9 @@ package com.example.ualaapp.repository.implementations.database.daos
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import com.example.ualaapp.repository.implementations.database.entities.CityEntity
 
 @Dao
@@ -11,7 +13,18 @@ interface CityDao {
     @Insert
     suspend fun insert(cityEntity: CityEntity)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(listOfCityEntities: List<CityEntity>)
+
+    @Query("DELETE FROM cities")
+    suspend fun cleanTable()
+
     @Query("SELECT * FROM cities ORDER BY name ASC")
     fun getAll(): List<CityEntity>
 
+    @Transaction
+    suspend fun refreshData(listOfCityEntities: List<CityEntity>) {
+        cleanTable()
+        insertAll(listOfCityEntities)
+    }
 }
