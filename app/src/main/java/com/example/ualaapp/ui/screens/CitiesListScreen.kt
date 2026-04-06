e02f027196fe3c79fb9145b8c780b1bb13d76aa2
Hernan L. Romero <romero.hernan.87@gmail.com>
2026-04-06 12:18:02 -0300

    clean import

--- app/src/main/java/com/example/ualaapp/ui/screens/CitiesListScreen.kt
+++ app/src/main/java/com/example/ualaapp/ui/screens/CitiesListScreen.kt
@@ -1,6 +1,5 @@
 package com.example.ualaapp.ui.screens
 
-import android.icu.text.CaseMap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
