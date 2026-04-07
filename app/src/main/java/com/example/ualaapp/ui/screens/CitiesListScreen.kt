d759927b744505e5be6f913eaa1542f3d654f0cb
Hernan L. Romero <romero.hernan.87@gmail.com>
2026-04-07 15:06:47 -0300

    keyboard options

--- app/src/main/java/com/example/ualaapp/ui/screens/CitiesListScreen.kt
+++ app/src/main/java/com/example/ualaapp/ui/screens/CitiesListScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,6 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -100,7 +102,11 @@ fun CitiesFilterComponent(
             Text(
                 text = stringResource(R.string.filter_city)
             )
-        }
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text
+        )
     )
 }
 
